a7e4293cac673030b88ed0fd78abc17270757a6a
Azedval <azedval@gmail.com>
2021-02-27 23:56:49 +0300

    first commit

---
+++ src/Car/Car.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const car = (props) => {
+    return (
+        <div>
+            <h3>Car name: {props.name}
+                </h3>
+                <p>
+                    Year: <strong>{props.year}</strong></p>
+                </div>
+    )
+}
+
+
+export default car;
